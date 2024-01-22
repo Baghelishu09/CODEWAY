1ace19e186dc60b8543c8c39753467d373764361
Ishu Baghel <baghelishu09@gmail.com>
2024-01-22 14:15:02 +0530

    modified code

--- task1_calculator.py
+++ task1_calculator.py
@@ -1,23 +1,101 @@
 from tkinter import *
 
 win = Tk()
-win.geometry("300x400+0+0")
-win.title("SIMPLE CALCULATOR")
-win.resizable(False,False)
-win.configure(background="white")
+win.title("Calculator")
+win.geometry("400x480+50+100")
+win.resizable(True,True)
 
-# functionality and logic
+
+# functionlity and logic
+
+global output_val
     
+def click(event):
+    text=event.widget.cget("text")
+    if(text=="C"):
+        pass
+    elif(text=="="):
+        pass
+    else:
+        output_val.get(output_val.set+text)
+        enty.update()
 
-#result screen
 
-result_scrn=Label(win,text=0,fg="black",bg="white")
-result_scrn.grid(row=0,column=0)
-result_scrn.config(font=("verdana",30,"bold"))
 
-btn1 = Button(win,text='0',width=5,height=2,
-              fg="grey",bg="white")
-btn1.grid(row=1,column=0)
-btn1.config(font=("calibri",15,"bold"))
+# OutPut Screen for Result
+
+output_val=StringVar()
+output_val.set("")
+
+output_frame=Frame(win,width=500,height=100,relief=RIDGE,bg="white"
+                   ,borderwidth=10)
+output_frame.pack(side=TOP,fill=X)
+
+#entry
+enty=Entry(output_frame,textvariable=output_val,fg="black",bg="white",
+           font=("times new roman",50,"bold"),justify='right',state='readonly')
+enty.pack(side=TOP,fill=BOTH)
+
+# Input Screen
+
+# frame
+
+input_frame=Frame(win,width=395,height=370,bg="light grey",
+                  relief=RIDGE)
+input_frame.place(x=0,y=110)
+
+#buttons
+
+btn = Button(input_frame,text="7",width=3,height=1,font=("Arial",30))
+btn.grid(row=0,column=0,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+btn = Button(input_frame,text="8",width=3,height=1,font=("Arial",30))
+btn.grid(row=0,column=1,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+btn = Button(input_frame,text="9",width=3,height=1,font=("Arial",30))
+btn.grid(row=0,column=2,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+btn = Button(input_frame,text="/",width=3,height=1,font=("Arial",30))
+btn.grid(row=0,column=3,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+
+btn = Button(input_frame,text="4",width=3,height=1,font=("Arial",30),padx=5)
+btn.grid(row=1,column=0,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+btn = Button(input_frame,text="5",width=3,height=1,font=("Arial",30),padx=5)
+btn.grid(row=1,column=1,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+btn = Button(input_frame,text="6",width=3,height=1,font=("Arial",30),padx=5)
+btn.grid(row=1,column=2,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+btn = Button(input_frame,text="*",width=3,height=1,font=("Arial",30),padx=5)
+btn.grid(row=1,column=3,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+
+btn = Button(input_frame,text="1",width=3,height=1,font=("Arial",30),padx=5)
+btn.grid(row=2,column=0,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+btn = Button(input_frame,text="2",width=3,height=1,font=("Arial",30),padx=5)
+btn.grid(row=2,column=1,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+btn = Button(input_frame,text="3",width=3,height=1,font=("Arial",30),padx=5)
+btn.grid(row=2,column=2,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+btn = Button(input_frame,text="-",width=3,height=1,font=("Arial",30),padx=5)
+btn.grid(row=2,column=3,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+
+btn = Button(input_frame,text="C",width=3,height=1,font=("Arial",30),padx=5)
+btn.grid(row=3,column=0)
+btn.bind("<Button-1>",click)
+btn = Button(input_frame,text="0",width=3,height=1,font=("Arial",30),padx=5)
+btn.grid(row=3,column=1,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+btn = Button(input_frame,text="=",width=3,height=1,font=("Arial",30),padx=5)
+btn.grid(row=3,column=2,padx=5,pady=5)
+btn.bind("<Button-1>",click)
+btn = Button(input_frame,text="+",width=3,height=1,font=("Arial",30),padx=5)
+btn.grid(row=3,column=3,padx=5,pady=5)
+btn.bind("<Button-1>",click)
 
 win.mainloop()
