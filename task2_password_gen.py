50e82d45fe4fcac25c41c8c1db5ad88fecf3d006
Ishu Baghel <baghelishu09@gmail.com>
2024-01-25 12:51:01 +0530

    change

--- task2_password_gen.py
+++ task2_password_gen.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import string
+import random
 
 win = Tk()
 win.title("Password Generator")
@@ -7,7 +9,15 @@ win.resizable(False,False)
 
 #functionality
 
+inputval=IntVar()
+outputval=StringVar()
 
+def reset(event):
+    inputval.set("")
+
+def generate(event):
+    outputval=inputval.set(inputval.get)
+        
 
 #gui
 
@@ -19,18 +29,20 @@ lbl1 = Label(win,text="Enter size of password :",fg="black",
              font=("times new roman",14))
 lbl1.place(x=10,y=70)
 
-input_field = Entry(win,font=("audela",15),borderwidth=2,
+input_field = Entry(win,textvariable=inputval,font=("audela",15),borderwidth=2,
                     relief=RIDGE)
 input_field.place(x=200,y=70)
 
 btn1=Button(win,text="GENERATE",font=("times new roman",10,"bold"),width=15,height=2,
             relief=RAISED,fg="white",bg="green")
 btn1.place(x=75,y=140)
+btn1.bind("<Button-1>",generate)
 btn2=Button(win,text="RESET",font=("times new roman",10,"bold"),width=18,height=2,
             relief=RAISED,fg="white",bg="green")
 btn2.place(x=230,y=140)
+btn2.bind("<Button-1>",reset)
 
-output_field=Entry(win,font=("times new roman",25,"bold"),bg="white",fg="black",
+output_field=Entry(win,textvariable=outputval,font=("times new roman",25,"bold"),bg="white",fg="black",
                    relief=SOLID,state='readonly',borderwidth=4)
 output_field.place(x=50,y=230)
 
